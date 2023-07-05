d77fe4755b4e6e316aa8f2229a463aebe6ac8de3
skdhar50 <skdhar50@gmail.com>
2023-07-06 00:02:27 +0600

    responsiveness added

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,16 +3,16 @@ import Service from "./Service";
 const Landing = () => {
   return (
     <div className="space-y-20">
-      <div className="head-section space-y-8">
-        <h1 className="px-40 leading-relaxed text-4xl font-bold text-center">
+      <div className="head-section pt-6 space-y-3 sm:space-y-4 md:space-y-8">
+        <h1 className=" sm:px-6 lg:px-28 xl:px-40 leading-relaxed text-xl sm:text-2xl md:text-3xl xl:text-4xl font-bold text-center">
           Explore Our Storage Space, Co-Working Space, Co-Warehouse & Services
         </h1>
-        <h5 className="text-xl text-center">
+        <h5 className="text-sm sm:text-md md:text-xl text-center">
           Carefully crafted for a hassle-free and affordable experience
         </h5>
       </div>
 
-      <div className="spaces grid grid-cols-4 gap-x-3 gap-y-16 px-14 pb-12">
+      <div className="spaces grid justify-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 md:gap-x-8 gap-y-16 sm:px-12 md:px-10 lg:px-14 pb-12">
         <Service />
         <Service />
         <Service />
--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -3,15 +3,15 @@ import { FaArrowRightLong } from "react-icons/fa6";
 
 const Service = () => {
   return (
-    <div className="space-y-5">
-      <img src={DoorToDoor} className="w-24" alt="document-storage" />
-      <div className="pb-6 space-y-1">
-        <h4 className="font-bold">Door to Door Storage</h4>
-        <p className="">We pick up, store & deliver</p>
+    <div className="space-y-3 sm:space-y-5">
+      <img src={DoorToDoor} className="w-36 sm:w-24" alt="document-storage" />
+      <div className="pb-2 space-y-1 sm:space-y-2">
+        <h4 className="text-md sm:text-lg font-bold">Door to Door Storage</h4>
+        <p className="text-sm sm:text-md">We pick up, store & deliver</p>
       </div>
       <a
         href="#"
-        className="text-sky-500 font-bold text-md flex justify-start items-center gap-3 cursor-pointer"
+        className="text-sky-500 font-bold text-sm sm:text-md flex justify-start items-center gap-3 cursor-pointer"
       >
         Learn More <FaArrowRightLong />
       </a>
